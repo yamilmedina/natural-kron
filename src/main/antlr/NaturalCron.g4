c4dde5b8a0e38c753da78a0a994690870d84efe2
yamilmedina <yamilmedina@users.noreply.github.com>
2024-05-04 22:30:14 +0200

    feat(antlr): extending grammar to time, and more tests

--- src/main/antlr/NaturalCron.g4
+++ src/main/antlr/NaturalCron.g4
@@ -8,11 +8,12 @@ options {
 naturalCron: date at clock;
 at: AT;
 date: STANDARD_SCHEDULE | (DYNAMIC_SCHEDULE DAYS | DYNAMIC_FROM_NOW_SCHEDULE);
-clock: HOUR;
+clock: FULL_HOUR | SHORT_HOUR;
 
 // Lexer Rules
-fragment INT: [0-9]+;
-HOUR: INT ':' INT;
+fragment INT: [0-9];
+FULL_HOUR: INT INT? ':' INT INT;
+SHORT_HOUR: [0-2]? INT? 'hr' 's'? ;
 DAYS: ('monday' | 'tuesday' | 'wednesday' | 'thursday' | 'friday' | 'saturday' | 'sunday');
 AT: ('at' | 'AT');
 STANDARD_SCHEDULE: ('hourly' | 'daily' | 'weekly' | 'yearly');
--- src/test/kotlin/io/github/yamilmedina/kron/NaturalKronAntlrGenParserTest.kt
+++ src/test/kotlin/io/github/yamilmedina/kron/NaturalKronAntlrGenParserTest.kt
@@ -4,66 +4,92 @@ import io.github.yamilmedina.kron.internal.antlr.NaturalCronLexer
 import io.github.yamilmedina.kron.internal.antlr.NaturalCronParser
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 class NaturalKronAntlrGenParserTest {
-    @Test
-    fun testAntlrParser_StandardSchedule() {
-        val input = CharStreams.fromString("weekly at 15:00")
-        val lexer = NaturalCronLexer(input)
-        val tokens = CommonTokenStream(lexer)
 
-        val parser = NaturalCronParser(tokens)
-        val naturalCron = parser.naturalCron()
+    @Nested
+    inner class StandardScheduleTestCases {
+        @Test
+        fun givenAStandardSchedule_whenParsing_ShouldReturnCorrectFormat() {
+            val input = CharStreams.fromString("weekly at 15:00")
+            val lexer = NaturalCronLexer(input)
+            val tokens = CommonTokenStream(lexer)
 
-        assertNotNull(naturalCron.date().STANDARD_SCHEDULE())
-        assertEquals("weekly", naturalCron.date().STANDARD_SCHEDULE().toString())
-        assertEquals("15:00", naturalCron.clock().HOUR().toString())
+            val parser = NaturalCronParser(tokens)
+            val naturalCron = parser.naturalCron()
+
+            assertNotNull(naturalCron.date().STANDARD_SCHEDULE())
+            assertEquals("weekly", naturalCron.date().STANDARD_SCHEDULE().toString())
+            assertEquals("15:00", naturalCron.clock().FULL_HOUR().toString())
+        }
     }
 
-    @Test
-    fun testAntlrParser_DynamicScheduleFromNow() {
-        val input = CharStreams.fromString("every day at 15:00")
-        val lexer = NaturalCronLexer(input)
-        val tokens = CommonTokenStream(lexer)
+    @Nested
+    inner class DynamicScheduleTestCases {
+        @Test
+        fun givenADynamicSchedule_whenParsingNoDaySpecified_ShouldReturnEveryFormat() {
+            val input = CharStreams.fromString("every day at 15:00")
+            val lexer = NaturalCronLexer(input)
+            val tokens = CommonTokenStream(lexer)
 
-        val parser = NaturalCronParser(tokens)
-        val naturalCron = parser.naturalCron()
+            val parser = NaturalCronParser(tokens)
+            val naturalCron = parser.naturalCron()
 
-        assertNotNull(naturalCron.date().DYNAMIC_FROM_NOW_SCHEDULE())
-        assertEquals("every day", naturalCron.date().DYNAMIC_FROM_NOW_SCHEDULE().toString())
-        assertEquals("15:00", naturalCron.clock().HOUR().toString())
+            assertNotNull(naturalCron.date().DYNAMIC_FROM_NOW_SCHEDULE())
+            assertEquals("every day", naturalCron.date().DYNAMIC_FROM_NOW_SCHEDULE().toString())
+            assertEquals("15:00", naturalCron.clock().FULL_HOUR().toString())
+        }
+
+        @Test
+        fun givenADynamicSchedule_whenParsing_ShouldReturnCorrectFormat() {
+            val input = CharStreams.fromString("every monday at 15:00")
+            val lexer = NaturalCronLexer(input)
+            val tokens = CommonTokenStream(lexer)
+
+            val parser = NaturalCronParser(tokens)
+            val naturalCron = parser.naturalCron()
+
+            assertNotNull(naturalCron.date().DYNAMIC_SCHEDULE())
+            assertEquals("every", naturalCron.date().DYNAMIC_SCHEDULE().toString())
+            assertEquals("monday", naturalCron.date().DAYS().toString())
+            assertEquals("15:00", naturalCron.clock().FULL_HOUR().toString())
+        }
+
+        @Test
+        fun givenAnInputWithMixedCase_whenParsing_ShouldReturnCorrectFormat() {
+            val input = CharStreams.fromString("eVery Monday at 15:00")
+            val lexer = NaturalCronLexer(input)
+            val tokens = CommonTokenStream(lexer)
+
+            val parser = NaturalCronParser(tokens)
+            val naturalCron = parser.naturalCron()
+
+            assertNotNull(naturalCron.date().DYNAMIC_SCHEDULE())
+            assertEquals("every", naturalCron.date().DYNAMIC_SCHEDULE().toString().lowercase())
+            assertEquals("Monday", naturalCron.date().DAYS().toString())
+            assertEquals("15:00", naturalCron.clock().FULL_HOUR().toString())
+        }
     }
 
-    @Test
-    fun testAntlrParser_DynamicScheduleDetail() {
-        val input = CharStreams.fromString("every monday at 15:00")
-        val lexer = NaturalCronLexer(input)
-        val tokens = CommonTokenStream(lexer)
+    @Nested
+    inner class HourScheduleTestCases {
+        @Test
+        fun givenAShortHourStyle_whenParsing_ShouldReturnCorrectFormat() {
+            val input = CharStreams.fromString("every Monday at 15hrs")
+            val lexer = NaturalCronLexer(input)
+            val tokens = CommonTokenStream(lexer)
 
-        val parser = NaturalCronParser(tokens)
-        val naturalCron = parser.naturalCron()
+            val parser = NaturalCronParser(tokens)
+            val naturalCron = parser.naturalCron()
 
-        assertNotNull(naturalCron.date().DYNAMIC_SCHEDULE())
-        assertEquals("every", naturalCron.date().DYNAMIC_SCHEDULE().toString())
-        assertEquals("monday", naturalCron.date().DAYS().toString())
-        assertEquals("15:00", naturalCron.clock().HOUR().toString())
-    }
-
-    @Test
-    fun testAntlrParser_ShouldIgnoreCase() {
-        val input = CharStreams.fromString("eVery Monday at 15:00")
-        val lexer = NaturalCronLexer(input)
-        val tokens = CommonTokenStream(lexer)
-
-        val parser = NaturalCronParser(tokens)
-        val naturalCron = parser.naturalCron()
-
-        assertNotNull(naturalCron.date().DYNAMIC_SCHEDULE())
-        assertEquals("every", naturalCron.date().DYNAMIC_SCHEDULE().toString().toLowerCase())
-        assertEquals("Monday", naturalCron.date().DAYS().toString())
-        assertEquals("15:00", naturalCron.clock().HOUR().toString())
+            assertNotNull(naturalCron.date().DYNAMIC_SCHEDULE())
+            assertEquals("every", naturalCron.date().DYNAMIC_SCHEDULE().toString().lowercase())
+            assertEquals("Monday", naturalCron.date().DAYS().toString())
+            assertEquals("15hrs", naturalCron.clock().SHORT_HOUR().toString())
+        }
     }
 }
